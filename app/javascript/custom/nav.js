242ee34468a8a7d68b1ec889e8b914fe56aabeab
Adam Bohannon <abo46n2@gmail.com>
2019-12-15 20:17:37 -0800

    Hover dropdown for account

--- app/javascript/custom/nav.js
+++ app/javascript/custom/nav.js
@@ -1,8 +1,12 @@
 document.addEventListener("turbolinks:load", () => {
+  mobileMenuToggle();
+});
+
+function mobileMenuToggle() {
   const mobileMenuButton = document.querySelector("#mobile-menu-button");
   const menuBody = document.querySelector("#menu-body");
 
   mobileMenuButton.addEventListener("click", () => {
     menuBody.classList.toggle("hidden");
   });
-});
+}
